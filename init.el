988750629ad07bb25b81b86edb2fb1753cfefc8e
Claynon Souza <claynon@gmail.com>
2018-01-09 16:51:08 +0100

    add basic modes to emacs

--- init.el
+++ init.el
@@ -1,11 +1,12 @@
-;; Melpa
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; MELPA ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;; (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  )
+  (add-to-list 'package-archives
+	       (cons "melpa" (concat proto "://melpa.org/packages/")) t))
 
 (package-initialize)
 
@@ -14,7 +15,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (guide-key helm)))
+ '(column-number-mode t)
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
+     default)))
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (diff-hl magit fuzzy auto-complete auto-highlight-symbol undo-tree bind-key
+     mwim which-key fill-column-indicator solarized-theme smartparens projectile
+     helm-projectile helm-swoop helm)))
  '(tool-bar-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -23,49 +34,198 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; Helm
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; HELM ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'helm)
 (require 'helm-config)
+(require 'bind-key)
 
-;; (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(bind-keys* ("M-x"     . helm-M-x)
+	    ("C-c h"   . helm-command-prefix)
+	    ("C-x C-f" . helm-find-files)
+	    ("C-x b"   . helm-buffers-list)
+	    ("C-x C-b" . switch-to-buffer))
 (global-unset-key (kbd "C-x c"))
+(which-key-add-key-based-replacements "C-c h" "Helm")
 
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
+; rebind tab to run persistent action
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+; make TAB work in terminal
+(define-key helm-map (kbd "C-i")   'helm-execute-persistent-action)
+; list actions using C-z
+(define-key helm-map (kbd "C-z")   'helm-select-action)
 
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
-(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-
-(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+; open helm buffer inside current window, not occupy whole other window
+(setq helm-split-window-in-side-p           t
+; move to end or beginning of source when reaching top or bottom of source.
+      helm-move-to-line-cycle-in-source     t
+; search for library in `require' and `declare-function' sexp.
+      helm-ff-search-library-in-sexp        t
       helm-ff-file-name-history-use-recentf t
-      helm-echo-input-in-header-line        t)
+      helm-echo-input-in-header-line        t
+      helm-M-x-fuzzy-match                  t
+      helm-boring-file-regexp-list          '("~$")
+      helm-ff-skip-boring-files             t
+      helm-move-to-line-cycle-in-source     nil)
 
-(setq helm-autoresize-max-height 0)
-(setq helm-autoresize-min-height 30)
-(helm-autoresize-mode 1)
+(setq helm-autoresize-max-height 0
+      helm-autoresize-min-height 30)
+(helm-autoresize-mode t)
 
 (helm-mode t)
 
-;; Guide key
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; HELM SWOOP ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'helm-swoop)
 
-(require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x" "C-c" "C-h" "ESC"))
-(setq guide-key/recursive-key-sequence-flag t)
-(setq guide-key/highlight-command-regexp "sexp")
-(setq guide-key/idle-delay 0.2)
-(setq guide-key/popup-window-position 'bottom)
-(guide-key-mode t)
+(bind-keys* ("C-s" . helm-swoop)
+	    ("C-r" . helm-swoop))
+(define-key helm-command-map (kbd "C-s") 'helm-multi-swoop)
+(setq helm-swoop-split-direction    'split-window-horizontally
+      helm-swoop-move-to-line-cycle t)
 
-;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; HELM PROJECTILE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'helm-projectile)
 
+(helm-projectile-on)
+(which-key-add-key-based-replacements "C-c p" "Projectile")
+(projectile-global-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; SMART PARENS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'smartparens)
+(require 'smartparens-config)
+
+(add-hook 'prog-mode-hook #'smartparens-mode)
+(bind-keys :prefix-map smartparens-mode-map
+	   :prefix "C-c l"
+	   ("s" . sp-forward-slurp-sexp)
+	   ("S" . sp-backward-slurp-sexp)
+	   ("b" . sp-forward-barf-sexp)
+	   ("B" . sp-backward-barf-sexp))
+(which-key-add-key-based-replacements "C-c l" "Lisp")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;; FILL COLUMN INDICATOR ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'fill-column-indicator)
+
+(setq fci-rule-column 80
+      fci-rule-width  2)
+(add-hook 'prog-mode-hook #'fci-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  WHITESPACE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'whitespace)
+
+(setq whitespace-style '(face lines-tail))
+(setq whitespace-line-column 80)
+(global-whitespace-mode t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; MWIM ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(bind-keys* ("C-a"    . mwim-beginning-of-code-or-line)
+	    ("<home>" . mwim-beginning-of-code-or-line)
+	    ("C-e"    . mwim-end-of-code-or-line)
+	    ("<end>"  . mwim-end-of-code-or-line))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; WHICH KEY ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'which-key)
+
+(bind-key* "M-m" 'which-key-show-major-mode)
+(setq which-key-side-window-max-height 0.2
+      which-key-idle-delay             0.1
+      which-key-add-column-padding     0)
+(which-key-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; UNDO TREE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-undo-tree-mode t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;; AUTO HIGHLIGHT SYMBOL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq ahs-idle-interval 0.2)
+(global-auto-highlight-symbol-mode t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; AUTO COMLETE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(ac-config-default)
+(setq ac-auto-show-menu 0.2)
+(setq ac-quick-help-delay 0.2)
+(setq ac-use-fuzzy t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; DIFF HIGHLIGHT ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-diff-hl-mode t)
+(diff-hl-flydiff-mode t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; MAGIT ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(bind-keys :prefix-map magit-mode-map
+	   :prefix     "C-c g"
+	   ("a" . magit-stage-file)
+	   ("c" . magit-commit)
+	   ("d" . magit-diff)
+	   ("s" . magit-status)
+	   ("u" . magit-unstage-file)
+	   ("C-s l" . magit-stash-list)
+	   ("C-s p" . magit-stash-popup)
+	   ("C-s s" . magit-stash))
+(which-key-add-key-based-replacements "C-c g" "Git")
+(which-key-add-key-based-replacements "C-c g C-s" "stash")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; EMACS LISP ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(which-key-add-major-mode-key-based-replacements 'emacs-lisp-mode
+  "C-c e" "Eval")
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "C-c e b") 'eval-buffer)
+	    (local-set-key (kbd "C-c e r") 'eval-buffer)))
+
+;; mode line
 (global-linum-mode t)
-;; (tool-bar-mode nil)
+(column-number-mode t)
+(size-indication-mode t)
+(display-battery-mode t)
+(display-time-mode t)
+
+;; Misc
+(load-theme 'solarized-dark t)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(save-place-mode t)
+(global-hl-line-mode t)
+
+(set-face-attribute 'default nil
+                    :family "Source Code Pro"
+                    :height 100
+                    :weight 'normal
+                    :width 'normal)
 
 
-;; helm projectile
-;; helm swoop
+;; barra inferior -- reddit share your mode-line customization
+;; ;; numero da coluna
+;; ;; nome do projeto?
+;; ;; percentual da p'agina
+;; clj
+;; scala
+;; autosave
+;; window navigation
+;; neotree
