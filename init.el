10677b362b6d05387ee85031ab6b93cd5db4ea70
Claynon Souza <claynon@gmail.com>
2018-01-11 22:37:52 +0100

    emacs: small tweaks + exec-path-from-shell

--- init.el
+++ init.el
@@ -15,17 +15,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode nil)
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
-    (cider-eval-sexp-fu aggressive-indent rainbow-delimiters clojure-mode
-     neotree winum centered-cursor-mode diff-hl magit fuzzy auto-complete
+    (exec-path-from-shell cider-eval-sexp-fu cider clj-refactor
+     aggressive-indent rainbow-delimiters clojure-mode neotree winum
+     centered-cursor-mode diff-hl magit fuzzy auto-complete
      auto-highlight-symbol undo-tree bind-key mwim which-key
-     fill-column-indicator solarized-theme smartparens projectile
+     fill-column-indicator solarized-theme smartparens markdown projectile
      helm-projectile helm-swoop helm)))
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -45,8 +45,7 @@
 (bind-keys* ("M-x"     . helm-M-x)
 	    ("C-c h"   . helm-command-prefix)
 	    ("C-x C-f" . helm-find-files)
-	    ("C-x b"   . helm-buffers-list)
-	    ("C-x C-b" . switch-to-buffer))
+	    ("C-x b"   . helm-buffers-list))
 (global-unset-key (kbd "C-x c"))
 (which-key-add-key-based-replacements "C-c h" "Helm")
 
@@ -68,13 +67,14 @@
       helm-M-x-fuzzy-match                  t
       helm-boring-file-regexp-list          '("~$")
       helm-ff-skip-boring-files             t
+      projectile-switch-project-action      'projectile-find-file
       helm-move-to-line-cycle-in-source     nil)
 
 (setq helm-autoresize-max-height 0
       helm-autoresize-min-height 30)
-(helm-autoresize-mode)
+(helm-autoresize-mode t)
 
-(helm-mode)
+(helm-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; HELM SWOOP ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -94,8 +94,11 @@
 
 (helm-projectile-on)
 (which-key-add-key-based-replacements "C-c p" "Projectile")
-(bind-key* "C-c p C-t" 'projectile-toggle-between-implementation-and-test)
-(projectile-global-mode)
+(global-unset-key (kbd "C-c p s"))
+(bind-keys* ("C-c p C-t" . projectile-toggle-between-implementation-and-test)
+	    ("C-c p s"   . helm-projectile-grep)
+	    ("C-x C-b"   . projectile-switch-to-buffer))
+(projectile-global-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; SMART PARENS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -106,10 +109,30 @@
 (add-hook 'prog-mode-hook #'smartparens-mode)
 (bind-keys :prefix-map smartparens-mode-map
 	   :prefix "C-c l"
-	   ("s" . sp-forward-slurp-sexp)
-	   ("S" . sp-backward-slurp-sexp)
-	   ("b" . sp-forward-barf-sexp)
-	   ("B" . sp-backward-barf-sexp))
+	   ("s"   . sp-forward-slurp-sexp)
+	   ("S"   . sp-backward-slurp-sexp)
+	   ("b"   . sp-forward-barf-sexp)
+	   ("B"   . sp-backward-barf-sexp)
+	   ("k"   . sp-kill-sexp)
+	   ("K"   . sp-backward-kill-sexp)
+	   ("u"   . sp-up-sexp)
+	   ("U"   . sp-backward-up-sexp)
+	   ("C-u" . sp-unwrap-sexp)
+	   ("r"   . sp-raise-sexp)
+	   ("<right>" . ahs-forward)
+	   ("<left>"  . ahs-backward))
+(bind-key "C-(" `(lambda (&optional arg)
+		   (interactive "P")
+		   (sp-wrap-with-pair "(")))
+(bind-key "ESC [" `(lambda (&optional arg)
+		     (interactive "P")
+		     (sp-wrap-with-pair "[")))
+(bind-key "C-{" `(lambda (&optional arg)
+		   (interactive "P")
+		   (sp-wrap-with-pair "{")))
+(bind-key "C-\"" `(lambda (&optional arg)
+		    (interactive "P")
+		    (sp-wrap-with-pair "\"")))
 (which-key-add-key-based-replacements "C-c l" "Lisp")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -128,7 +151,7 @@
 
 (setq whitespace-style '(face lines-tail))
 (setq whitespace-line-column 80)
-(global-whitespace-mode)
+(global-whitespace-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; MWIM ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -147,18 +170,18 @@
 (setq which-key-side-window-max-height 0.2
       which-key-idle-delay             0.1
       which-key-add-column-padding     0)
-(which-key-mode)
+(which-key-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; UNDO TREE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(global-undo-tree-mode)
+(global-undo-tree-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; AUTO HIGHLIGHT SYMBOL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq ahs-idle-interval 0.2)
-(global-auto-highlight-symbol-mode)
+(global-auto-highlight-symbol-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; AUTO COMPLETE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -171,14 +194,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; DIFF HIGHLIGHT ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(global-diff-hl-mode)
+(global-diff-hl-mode t)
 (diff-hl-flydiff-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; CENTERED CURSOR  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'centered-cursor-mode)
-(global-centered-cursor-mode)
+(global-centered-cursor-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; WINUM  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -208,8 +231,8 @@
 (add-to-list 'winum-assign-functions #'winum-assign-0-to-neotree)
 (setq winum-ignored-buffers '(" *which-key*"))
 
-(winum-mode)
-(winner-mode)
+(winum-mode t)
+(winner-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; NEOTREE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -225,25 +248,55 @@
       (neotree-find origin-buffer-file-name))))
 (bind-key* "C-c p t" 'neotree-find-project-root)
 
-(setq projectile-switch-project-action 'neotree-projectile-action
-      neo-smart-open                   t
+(put 'car 'lisp-indent-function 0)
+(put 'last 'lisp-indent-function 0)
+(put 'butlast 'lisp-indent-function 0)
+(put 'split-string 'lisp-indent-function 0)
+(setq neo-smart-open                   t
       neo-create-file-auto-open        t
       neo-window-width                 32
       ;; neo-theme                        (if (display-graphic-p) 'icons 'arrow)
-      neo-banner-message               "Press ? for neotree help"
+      neo-banner-message               (concat "Project: "
+					       (car
+						 (last
+						   (butlast
+						     (split-string
+						       (projectile-project-info)
+						       "/")))))
       neo-show-updir-line              nil
       neo-mode-line-type               'neotree
       neo-show-hidden-files            nil
       neo-auto-indent-point            t
       neo-vc-integration               '(face))
+(eval-after-load "neotree"
+  '(defun neo-path--shorten (path len)
+     (make-string len ?-)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; AUTO-SAVE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq auto-save-default           t
-      auto-save-visited-file-name t
-      auto-save-interval          20
-      auto-save-timeout           1)
+;; (setq auto-save-default           t
+;;       auto-save-visited-file-name t
+;;       auto-save-interval          20
+;;       auto-save-timeout           1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;; EXEC PATH FROM SHELL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq exec-path-from-shell-check-startup-files nil)
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+(exec-path-from-shell-copy-envs '("NU_HOME"
+                                  "NUCLI_HOME"
+                                  "HOME"
+				  "TERM"
+                                  "AWS_DEFAULT_REGION"
+                                  "AWS_ACCESS_KEY"
+                                  "AWS_SECRET_ACCESS_KEY"
+                                  "AWS_ACCESS_KEY_ID"
+                                  "AWS_SECRET_KEY"
+                                  "ARTIFACTS_AWS_ACCESS_KEY_ID"
+                                  "ARTIFACTS_AWS_SECRET_ACCESS_KEY"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; MAGIT ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -270,6 +323,7 @@
   "C-c e" "Eval")
 (add-hook 'emacs-lisp-mode-hook
 	  (lambda ()
+	    (rainbow-delimiters-mode t)
 	    (local-set-key (kbd "C-c e b") 'eval-buffer)
 	    (local-set-key (kbd "C-c e r") 'eval-buffer)))
 
@@ -287,38 +341,67 @@
 (setq clojure-indent-style              :align-arguments
       clojure-align-forms-automatically t)
 (defun my-clojure-mode-hook ()
-  (subw
-   ord-mode)
-  (smartparens-strict-mode)
-  (rainbow-delimiters-mode))
+  (subword-mode t)
+  (smartparens-strict-mode t)
+  (rainbow-delimiters-mode t)
+  (auto-highlight-symbol-mode t))
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
 ;;;;;;;;; clj-refactor
 (require 'clj-refactor)
 
 (defun my-clj-refactor-hook ()
-  (clj-refactor-mode)
-  (yas-minor-mode)
-  (cljr-add-keybindings-with-prefix "C-c C-c"))
+  (clj-refactor-mode t)
+  (yas-minor-mode t)
+  (cljr-add-keybindings-with-prefix "C-c c R"))
+(which-key-add-major-mode-key-based-replacements 'clojure-mode
+  "C-c c R" "Refactor")
 (add-hook 'clojure-mode-hook #'my-clj-refactor-hook)
 
 ;;;;;;;;; cider
-(require 'cider-eval-sexp-fu)
+;; (require 'cider-eval-sexp-fu)
+
+(setq cider-save-file-on-load nil)
+(which-key-add-major-mode-key-based-replacements 'clojure-mode
+  "C-c c" "Clojure")
+(which-key-add-major-mode-key-based-replacements 'clojure-mode
+  "C-c c s" "Repl")
+(which-key-add-major-mode-key-based-replacements 'clojure-mode
+  "C-c c g" "Go to")
+(add-hook 'clojure-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "C-c c b") 'cider-load-buffer)
+	    (local-set-key (kbd "C-c c e") 'cider-eval-sexp-at-point)
+	    (local-set-key (kbd "C-c c n") 'cider-eval-ns-form)
+	    (local-set-key (kbd "C-c c r") 'cider-eval-region)
+	    (local-set-key (kbd "C-c c t") 'cljr-thread-first-all)
+	    (local-set-key (kbd "C-c c T") 'cljr-thread-last-all)
+	    (local-set-key (kbd "C-c c g b") 'cider-pop-back)
+	    (local-set-key (kbd "C-c c g g") 'cider-find-var)
+	    (local-set-key (kbd "C-c c g n") 'cider-find-ns)
+	    (local-set-key (kbd "C-c c s c") 'cider-connect)
+	    (local-set-key (kbd "C-c c s i") 'cider-jack-in)
+	    ;; cider-jack-in requires ipv6 to be enabled or change
+	    ;; cider-lein-parameters to use localhost instead of ::
+	    (local-set-key (kbd "C-c c s s") 'cider-switch-to-repl-buffer)))
+(add-hook 'repl-mode
+	  (lambda ()
+	    (local-set-key (kbd "C-c c c") 'cider-repl-clear-buffer)))
 
 ;; mode line
-(global-linum-mode)
-(column-number-mode)
-(size-indication-mode)
-(display-battery-mode)
-(display-time-mode)
+(global-linum-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+(display-battery-mode t)
+(display-time-mode t)
 
 ;; Misc
 (load-theme 'solarized-dark t)
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
-(save-place-mode)
-(global-hl-line-mode)
+(save-place-mode t)
+(global-hl-line-mode t)
 
 (set-face-attribute 'default nil
                     :family "Source Code Pro"
@@ -326,6 +409,7 @@
                     :weight 'normal
                     :width 'normal)
 
+(bind-key "M-j" 'join-line)
 
 ;; barra inferior -- reddit share your mode-line customization
 ;; ;; numero da coluna
@@ -338,3 +422,11 @@
 ;; pop up thing of the auto-complete
 ;; <BACKSPACE> in line with only <SPC>s does #'join-line
 ;; hydra?
+;; fix cider-eval-sexp-fu
+;; figure out how to use shell
+;; fix auto-save
+;; repl-mode
+;; y instead of yes
+;; add M-m or something like that
+;; clojure auto complete on popup, not minibuffer
+;; check if can use bind-keys with prefix-map
